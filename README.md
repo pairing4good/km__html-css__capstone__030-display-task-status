04fafc448fd8d3dbb84c5d8ebda3d344269eacbd
David Kessler <39646674+pairing4good@users.noreply.github.com>
2022-11-03 11:14:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 029 List Task Descriptions
+
+[![029 Task App](https://img.youtube.com/vi/yTrDWBbl3E4/0.jpg)](https://www.youtube.com/watch?v=yTrDWBbl3E4)
+
+[![030 User Stories](https://img.youtube.com/vi/uROJmuBtiJ0/0.jpg)](https://www.youtube.com/watch?v=uROJmuBtiJ0)
+
+```
+Given that three tasks exist  
+When I load the task app  
+Then I will see all three tasks listed on separate lines
+```
+
+Steps:
+- Add three tasks to the tasks div element.
+- Identify each task with the unique id's of task-description-1 , task-description-2, task-description-3 .
+
+Resources:
+- https://www.w3schools.com/html/html_id.asp
---
+++ test/verify.test.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/.." + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+describe('the tasks div', () => {
+  it('should contain three task items', async () => {
+    const elements = await page.$$(`[id^="task-description-"]`);
+    
+    expect(elements.length).toBe(3);
+  });
+});
